18beeb54d16bf9a6b03a0686b20be2e4543d1ad6
Shimada666 <649940882@qq.com>
2021-10-19 14:03:03 +0800

    feat: improve typing hint

--- stream.py
+++ stream.py
@@ -1,41 +1,44 @@
-from typing import TypeVar, Callable, List, Set, Generic, Dict, Any
+from typing import TypeVar, Callable, List, Set, Generic, Dict, Iterator
 
 T = TypeVar('T')
+R = TypeVar('R')
+K = TypeVar('K')
+U = TypeVar('U')
 
 
 class Stream(Generic[T]):
-    def __init__(self, list_pointer: List[T]):
-        self._list_pointer = list_pointer
+    def __init__(self, stream: Iterator[T] = None):
+        if stream:
+            self._stream = stream
+        else:
+            self._stream = iter([])
 
-    def map(self, func):
-        self._list_pointer = map(func, self._list_pointer)
-        return self
+    def map(self, func: Callable[[T], R]) -> 'Stream'[R]:
+        return Stream(map(func, self._stream))
 
-    def filter(self, func: Callable[[Any], bool]):
-        self._list_pointer = filter(func, self._list_pointer)
-        return self
+    def filter(self, func: Callable[[T], bool]) -> 'Stream'[T]:
+        return Stream(filter(func, self._stream))
 
-    def for_each(self, func: Callable) -> None:
-        for i in self._list_pointer:
+    def for_each(self, func: Callable[[T], None]) -> None:
+        for i in self._stream:
             func(i)
 
-    def peek(self, func: Callable):
-        for i in self._list_pointer:
+    def peek(self, func: Callable[[T], None]) -> 'Stream'[T]:
+        for i in self._stream:
             func(i)
         return self
 
-    def sorted(self):
-        self._list_pointer = sorted(self._list_pointer)
-        return self
+    def sorted(self) -> 'Stream'[T]:
+        return Stream(sorted(self._stream))
 
-    def count(self):
-        return len(list(self._list_pointer))
+    def count(self) -> int:
+        return len(list(self._stream))
 
-    def to_list(self):
-        return list(self._list_pointer)
+    def to_list(self) -> List[T]:
+        return list(self._stream)
 
-    def to_set(self) -> Set:
-        return set(self._list_pointer)
+    def to_set(self) -> Set[T]:
+        return set(self._stream)
 
-    def to_map(self, k: Callable, v: Callable) -> Dict:
-        return {k(i): v(i) for i in self._list_pointer}
+    def to_map(self, k: Callable[[T], K], v: Callable[[T], U]) -> Dict[K, U]:
+        return {k(i): v(i) for i in self._stream}
