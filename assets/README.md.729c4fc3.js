9ce78fc4cf86e9491ebce0925ffa896f36339458
Shimada666 <649940882@qq.com>
2022-08-21 17:20:59 +0800

    Updates

--- assets/README.md.729c4fc3.js
+++
@@ -1,6 +0,0 @@
-import{_ as a,c as s,o as e,a as n}from"./app.976719eb.js";const m=JSON.parse('{"title":"Document","description":"","frontmatter":{},"headers":[{"level":2,"title":"Usage","slug":"usage"}],"relativePath":"README.md","lastUpdated":null}'),t={name:"README.md"},l=n(`<h1 id="document" tabindex="-1">Document <a class="header-anchor" href="#document" aria-hidden="true">#</a></h1><h2 id="usage" tabindex="-1">Usage <a class="header-anchor" href="#usage" aria-hidden="true">#</a></h2><div class="language-shell"><button class="copy"></button><span class="lang">shell</span><pre><code><span class="line"><span style="color:#676E95;"># \u5B89\u88C5</span></span>
-<span class="line"><span style="color:#A6ACCD;">pnpm i</span></span>
-<span class="line"></span>
-<span class="line"><span style="color:#676E95;"># \u542F\u52A8</span></span>
-<span class="line"><span style="color:#A6ACCD;">pnpm dev</span></span>
-<span class="line"></span></code></pre></div>`,3),c=[l];function o(p,r,d,i,_,h){return e(),s("div",null,c)}const g=a(t,[["render",o]]);export{m as __pageData,g as default};
